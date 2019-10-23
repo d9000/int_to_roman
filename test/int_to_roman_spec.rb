5e3196440d89ca7731c02667a3f8c625eee02b11
Daniel Leguizamon <dani9000@pop-os.localdomain>
2019-10-23 17:02:26 -0300

    Added a test

--- test/int_to_roman_spec.rb
+++ test/int_to_roman_spec.rb
@@ -2,8 +2,15 @@ require 'int_to_roman'
 require 'rspec'
 
 describe Integer do
+	
+	it 'return a string' do
+		roman = 3549.to_roman
+		expect(roman).to be_kind_of(String)
+	end 
+
 	it 'converts an integer to roman' do
 		roman = 3549.to_roman
 		expect(roman).to eq('MMMDXLIX')
 	end
+
 end
